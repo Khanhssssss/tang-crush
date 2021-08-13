c74aa40075a757d780c0d770a4918b0ed3d6b73b
Khanhssssss <88871076+Khanhssssss@users.noreply.github.com>
2021-08-13 18:20:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Béo à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+   mà sao m lại luộc trứng không nước 
+  
+   nhưng đằng sau đây là những lời yêu thương để của t để động viên và góp phần lạc quan cho m đó =))`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    title: 'Phải chăng t đã thấy yên thưn ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng t đã mê mẩn tài nấu ăn của m  ',
+    btnYes: 'tuy nhiên vẫn cứ lầ yêu quý m nhất <33',
+    btnNo: 'bạn thân hơn 10 năm ạ :v',
+    question:'Trên thế giới hơn 7 tỉ người mà sao t lại quen đc m mới hay:))',
+    btnReply: 'Gửi cho cậu=)))',
+    reply: 'yêu thưn <33333333',
+    mess: 't còn muốn chơi với m dài dài cơ :>',
+    messDesc: 'yêu thương t thêm tí vào ko t sầu lắm:((.',
+    btnAccept: 'Okiiiii:>>>',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
